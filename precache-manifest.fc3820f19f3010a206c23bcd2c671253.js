1ff04e040b1b7656fc61c16acf217d431700e7d4
Junghyun Kim <conankunioi@gmail.com>
2020-10-19 23:27:53 -0700

    Updates

---
+++ precache-manifest.fc3820f19f3010a206c23bcd2c671253.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e8f1d5cbe99dad709fb7c9cddfba959",
+    "url": "/kseaseattle/index.html"
+  },
+  {
+    "revision": "23dea83d028e57c494f9",
+    "url": "/kseaseattle/static/css/main.008e13d7.chunk.css"
+  },
+  {
+    "revision": "b4fef6b4001063b2e9a5",
+    "url": "/kseaseattle/static/js/2.d55f8210.chunk.js"
+  },
+  {
+    "revision": "7d01b9f6fbfab2334963863ca0313ca8",
+    "url": "/kseaseattle/static/js/2.d55f8210.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23dea83d028e57c494f9",
+    "url": "/kseaseattle/static/js/main.8f69c750.chunk.js"
+  },
+  {
+    "revision": "9d468e7cfc6a223310ed",
+    "url": "/kseaseattle/static/js/runtime-main.e56f5f88.js"
+  }
+]);
